7ea25216deceeeb2d23c6e22d91f07300c085c2e
mateusgeraldoc <mateusgeraldocunha@gmail.com>
2024-12-28 16:24:40 -0300

    expressions

--- MyFirstProgram/src/main/Main.java
+++ MyFirstProgram/src/main/Main.java
@@ -8,7 +8,7 @@ public class Main {
 		//scanner
 		Scanner scanner = new Scanner(System.in);
 		
-		//print and variables
+		//print, variables and user input
 		System.out.println("What is your name?");
 		String name = scanner.nextLine();
 		System.out.println("How old are you?");
@@ -17,9 +17,15 @@ public class Main {
 		System.out.println("What your favorite color?");
 		String color = scanner.nextLine();
 		
+		//expressions
+		age++;
+		age--;
+		
 		//console:
 		System.out.println("Name: "+name);
 		System.out.println("Age: "+age);
 		System.out.println("Favorite Color: "+color);
+		
+		
 	}
 }
